13341d913bcbc13daf57767e433cb9ae618dc7ec
rossmat <mathieu.rosser@he-arc.ch>
2015-03-02 17:51:22 +0100

    Code de base balancier pour détection par motif SIFT

--- balancier/main.py
+++ balancier/main.py
@@ -9,47 +9,196 @@ Created on 16 févr. 2015
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+from common import anorm, getsize
 
-def manage_frame(frame):
-    
-    template = cv2.imread("../images et patterns/fixed.png")
-    w, h = template.shape[:-1]
-    
-    print(w)
-    print(h)
-    
-#     img = cv2.imread('messi5.jpg',0)
-#     img2 = img.copy()
-#     template = cv2.imread('template.jpg',0)
-#     w, h = template.shape[::-1]
-    
-    # All the 6 methods for comparison in a list
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-                'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+FLANN_INDEX_KDTREE = 1  # bug: flann enums are missing
+FLANN_INDEX_LSH    = 6
 
-    for meth in methods:
-        img = frame.copy()
-        method = eval(meth)
-    
-        # Apply template Matching
-        res = cv2.matchTemplate(img,template,method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            top_left = min_loc
+
+def init_feature(name):
+    chunks = name.split('-')
+    if chunks[0] == 'sift':
+        detector = cv2.SIFT()
+        norm = cv2.NORM_L2
+    elif chunks[0] == 'surf':
+        detector = cv2.SURF(800)
+        norm = cv2.NORM_L2
+    elif chunks[0] == 'orb':
+        detector = cv2.ORB(400)
+        norm = cv2.NORM_HAMMING
+    elif chunks[0] == 'akaze':
+        detector = cv2.AKAZE()
+        norm = cv2.NORM_HAMMING
+    elif chunks[0] == 'brisk':
+        detector = cv2.BRISK()
+        norm = cv2.NORM_HAMMING
+    else:
+        return None, None
+    if 'flann' in chunks:
+        if norm == cv2.NORM_L2:
+            flann_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
         else:
-            top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
+            flann_params= dict(algorithm = FLANN_INDEX_LSH,
+                               table_number = 6, # 12
+                               key_size = 12,     # 20
+                               multi_probe_level = 1) #2
+        matcher = cv2.FlannBasedMatcher(flann_params, {})  # bug : need to pass empty dict (#1329)
+    else:
+        matcher = cv2.BFMatcher(norm)
+    return detector, matcher
 
-        cv2.rectangle(img,top_left, bottom_right, 255, 2)
+
+def filter_matches(kp1, kp2, matches, ratio = 0.75):
+    mkp1, mkp2 = [], []
+    for m in matches:
+        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+            m = m[0]
+            mkp1.append( kp1[m.queryIdx] )
+            mkp2.append( kp2[m.trainIdx] )
+    p1 = np.float32([kp.pt for kp in mkp1])
+    p2 = np.float32([kp.pt for kp in mkp2])
+    kp_pairs = zip(mkp1, mkp2)
+    return p1, p2, kp_pairs
+
+def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
+    # Attention fuite mémoire!
     
-        cv2.imshow("Matching Result", res)
-        cv2.imshow("Detected Point", img)
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    vis = np.zeros((max(h1, h2), w1+w2), np.uint8)
+    vis[:h1, :w1] = img1
+    vis[:h2, w1:w1+w2] = img2
+    vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+
+    if H is not None:
+        corners = np.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
+        corners = np.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
+        cv2.polylines(vis, [corners], True, (255, 255, 255))
+
+    if status is None:
+        status = np.ones(len(kp_pairs), np.bool_)
+    p1 = np.int32([kpp[0].pt for kpp in kp_pairs])
+    p2 = np.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
+
+    green = (0, 255, 0)
+    red = (0, 0, 255)
+    white = (255, 255, 255)
+    kp_color = (51, 103, 236)
+    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+        if inlier:
+            col = green
+            cv2.circle(vis, (x1, y1), 2, col, -1)
+            cv2.circle(vis, (x2, y2), 2, col, -1)
+        else:
+            col = red
+            r = 2
+            thickness = 3
+            cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
+            cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
+            cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
+            cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
+    vis0 = vis.copy()
+    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+        if inlier:
+            cv2.line(vis, (x1, y1), (x2, y2), green)
+
+    cv2.imshow(win, vis)
+    
+    # Fuite mémoire !
+    
+#     def onmouse(event, x, y, flags, param):
+#         cur_vis = vis
+#         if flags & cv2.EVENT_FLAG_LBUTTON:
+#             cur_vis = vis0.copy()
+#             r = 8
+#             m = (anorm(p1 - (x, y)) < r) | (anorm(p2 - (x, y)) < r)
+#             idxs = np.where(m)[0]
+#             kp1s, kp2s = [], []
+#             for i in idxs:
+#                 (x1, y1), (x2, y2) = p1[i], p2[i]
+#                 col = (red, green)[status[i]]
+#                 cv2.line(cur_vis, (x1, y1), (x2, y2), col)
+#                 kp1, kp2 = kp_pairs[i]
+#                 kp1s.append(kp1)
+#                 kp2s.append(kp2)
+#             cur_vis = cv2.drawKeypoints(cur_vis, kp1s, flags=4, color=kp_color)
+#             cur_vis[:,w1:] = cv2.drawKeypoints(cur_vis[:,w1:], kp2s, flags=4, color=kp_color)
+# 
+#         cv2.imshow(win, cur_vis)
+#     cv2.setMouseCallback(win, onmouse)
+    return vis
+
+# def manage_frame(frame):
+#     
+#     template = cv2.imread("../images et patterns/fixed.png")
+#     w, h = template.shape[:-1]
+#     
+#     print(w)
+#     print(h)
+#     
+# #     img = cv2.imread('messi5.jpg',0)
+# #     img2 = img.copy()
+# #     template = cv2.imread('template.jpg',0)
+# #     w, h = template.shape[::-1]
+#     
+#     # All the 6 methods for comparison in a list
+#     methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+#                 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+# 
+#     for meth in methods:
+#         img = frame.copy()
+#         method = eval(meth)
+#     
+#         # Apply template Matching
+#         res = cv2.matchTemplate(img,template,method)
+#         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+#     
+#         # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+#         if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+#             top_left = min_loc
+#         else:
+#             top_left = max_loc
+#         bottom_right = (top_left[0] + w, top_left[1] + h)
+# 
+#         cv2.rectangle(img,top_left, bottom_right, 255, 2)
+#     
+#         cv2.imshow("Matching Result", res)
+#         cv2.imshow("Detected Point", img)
+
+def manage_frame(frame, ref):
+    detector, matcher = init_feature("sift")
+    
+    kp1, desc1 = detector.detectAndCompute(ref, None)
+    kp2, desc2 = detector.detectAndCompute(frame, None)
+    
+    def match_and_draw(win):
+        print 'matching...'
+        
+        if desc1 is not None and desc2 is not None:
+            raw_matches = matcher.knnMatch(desc1, trainDescriptors = desc2, k = 2) #2
+            p1, p2, kp_pairs = filter_matches(kp1, kp2, raw_matches)
+            if len(p1) >= 4:
+                H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+                print '%d / %d  inliers/matched' % (np.sum(status), len(status))
+            else:
+                H, status = None, None
+                print '%d matches found, not enough for homography estimation' % len(p1)
+    
+            if len(kp_pairs) > 0:
+                vis = explore_match(win, ref, frame, kp_pairs, status, H)
+
+    match_and_draw('find_obj')
+
 
 def read_video(video_file):
+    
     cap = cv2.VideoCapture(video_file)
 
+    ref = cv2.imread("FixedSmall.png", 0)
+    
+    import time
+    
     while cap.isOpened():
         ret, frame = cap.read()
         
@@ -58,7 +207,7 @@ def read_video(video_file):
         
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-        manage_frame(gray)
+        manage_frame(gray, ref)
         
         cv2.imshow('frame', gray)
         
@@ -70,4 +219,6 @@ def read_video(video_file):
 
 
 if __name__ == '__main__':
-    read_video('../video/exemple.mp4')
+    video = '../video/exemple.mp4'
+    video = 1
+    read_video(video)
